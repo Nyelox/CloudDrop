476d0bed8d8cca4f21f657ba3cec7cebfca7e582
Nyelox <59697534+Nyelox@users.noreply.github.com>
2025-11-15 23:54:26 +0200

    change auth

--- pyqt5/Server/Database_connection.py
+++ pyqt5/Server/Database_connection.py
@@ -1,11 +1,14 @@
 import pymysql
 from .auth import userauth
 
-
 auth = userauth()
 
 
 def handle_login(username, password):
+    # Check if account is locked
+    locked, lock_message = auth.is_locked(username)
+    if locked:
+        return lock_message
 
     con = pymysql.connect(
         host='localhost',
@@ -26,10 +29,14 @@ def handle_login(username, password):
         if isinstance(stored_hash, str):
             stored_hash = stored_hash.encode('utf-8')
         if auth.check_password(password, stored_hash):
+            if username in auth.login_attempts:
+                del auth.login_attempts[username]
             return "Login successful"
         else:
+            auth.track_failed_attempt(username)
             return "Invalid username or password"
     else:
+        auth.track_failed_attempt(username)
         return "Invalid username or password"
 
 
@@ -49,8 +56,11 @@ def handle_signup(username, password):
         response = "Username already exists"
     else:
         pwd_hash = auth.hash_password(password)
+
+
         if isinstance(pwd_hash, bytes):
             pwd_hash = pwd_hash.decode('utf-8')
+
         mycursor.execute('INSERT INTO data(username, password) VALUES (%s, %s)', (username, pwd_hash))
         con.commit()
         response = "Sign Up successful"
