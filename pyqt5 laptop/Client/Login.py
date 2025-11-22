53f933007cdf2bffb8f88c5e3d84e87c9f5406e0
Nyelox <59697534+Nyelox@users.noreply.github.com>
2025-11-22 22:11:50 +0200

    ui

---
+++ pyqt5 laptop/Client/Login.py
@@ -0,0 +1,107 @@
+import sys
+
+from Server import Database_connection
+
+import threading
+import queue
+import time
+
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QMessageBox
+from PyQt5.uic import loadUi
+from PyQt5.QtCore import Qt, QObject, pyqtSignal
+
+
+class WorkerThread(threading.Thread):
+    def __init__(self, task_queue, signals):
+        super().__init__(daemon=True)
+        self.task_queue = task_queue
+        self.signals = signals
+
+    def run(self):
+        while True:
+            try:
+                task_name, data = self.task_queue.get()
+                if task_name == "login":
+                    self.handle_login(data)
+            except Exception as e:
+                print(f"Error in worker: {e}")
+
+    def handle_login(self, data):
+        username, password = data
+
+        try:
+            message = Database_connection.handle_login(username, password)
+
+            if message == "Login successful":
+                self.signals.success.emit(username)
+            else:
+                self.signals.error.emit(message)
+
+        except Exception as e:
+            self.signals.error.emit(f"Error: {str(e)}")
+
+
+class WorkerSignals(QObject):
+    success = pyqtSignal(str)
+    error = pyqtSignal(str)
+
+
+class Login(QMainWindow):
+    def __init__(self):
+        super(Login, self).__init__()
+        loadUi("login.ui", self)
+
+        self.task_queue = queue.Queue()
+
+        self.signals = WorkerSignals()
+        self.signals.success.connect(self.on_login_success)
+        self.signals.error.connect(self.on_login_failed)
+
+        self.worker = WorkerThread(self.task_queue, self.signals)
+        self.worker.start()
+
+        self.pushButton_login.clicked.connect(self.login_function)
+        self.lineEdit_password.setEchoMode(QtWidgets.QLineEdit.Password)
+
+        self.label_signup = QLabel('<a href="#" style="text-decoration: none; color: #0066cc;">Register</a>', self)
+        self.label_signup.setTextFormat(Qt.RichText)
+        self.label_signup.linkActivated.connect(self.open_signup)
+        self.label_signup.setGeometry(225, 211, 141, 16)
+
+        self.home_window = None
+
+    def login_function(self):
+        username = self.lineEdit_userName.text().strip()
+        password = self.lineEdit_password.text()
+
+        if not username or not password:
+            self.show_message("All fields are required", QMessageBox.Warning)
+            return
+
+        self.task_queue.put(("login", (username, password)))
+
+    def on_login_success(self, username):
+        self.show_message(f"Welcome, {username}!", QMessageBox.Information)
+        self.open_home_window()
+    def open_home_window(self):
+        from home import Home
+        self.home_window = Home()
+        self.home_window.show()
+        self.close()
+
+    def on_login_failed(self, message):
+        self.show_message(message, QMessageBox.Critical)
+
+    def show_message(self, message, icon=QMessageBox.Information):
+        msg = QMessageBox(self)
+        msg.setIcon(icon)
+        msg.setText(message)
+        msg.setWindowTitle("Login")
+        msg.exec_()
+
+    def open_signup(self):
+        from signup import Signup
+        self.signup_window = Signup()
+        self.signup_window.show()
+        self.close()
