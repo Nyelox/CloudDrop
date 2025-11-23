67cbce6c81defd3c4b85dda4cb75d931578a5361
Nyelox <59697534+Nyelox@users.noreply.github.com>
2025-11-23 16:23:16 +0200

    Delete pyqt5 laptop directory

--- pyqt5 laptop/Client/Sendfile.py
+++
@@ -1,9 +0,0 @@
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QMainWindow
-from PyQt5.uic import loadUi
-
-
-class SendFile(QMainWindow):
-   def __init__(self):
-       super().__init__()
-       loadUi("sendfile.ui", self)
